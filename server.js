d1f4ad12c6e0985f6c1b29b0635d47886ac24877
larry laconi <soverylarry@gmail.com>
2020-07-23 18:19:27 -0400

    added app.post and delete setups

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express(); //start an express app
 const path = require("path"); // relate paths windows or mac
-const bodyParser = require("body-parser");
+//const bodyParser = require("body-parser");
 const fs = require("fs");
 const PORT = process.env.PORT || 8000;
 //const router = express.Router(); //always needs further configuration
@@ -11,20 +11,12 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/Develop/public")));
+//app.use(bodyParser);
 
+let dBase = fs.readFileSync("./Develop/db/db.json","utf-8");
+console.log("database:", dBase)
+dBase ? dBase = JSON.parse(dBase) : dBase = [];
 
-
-//Get Static files on server like: HTML, CSS, JS, etc.
-// app.get("/index", (req, res) => {
-//     res.json("you are viewing Express");
-// })
-
-// app.post('/index', (req, res)=>{
-//     console.log("Req: ", req);
-//     //req to receive stuff from the client/browser
-//     res.json({ msg:""});
-//     //res to send from Server to Client
-// })
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
@@ -38,16 +30,44 @@ app.get("/notes", (req, res) => {
       console.log("index:", __dirname);
   });
 
+// set API Routes
+app.get('/api/notes', (req, res) => {
+  return res.send(dBase);
+})
 
+// post user notes that are new and handle the data
+app.post("/api/notes", (req, res) => {
+  let body = req.body;
+  let uniqueId = {"id":Math.round(Math.random()* 999)};
+  
+  console.log("id:", uniqueId)
+  console.log("request:", body)
+  
+  body = {...uniqueId, ...body};
+  dBase.push(body);
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(dBase), "utf-8");
+  res.json(true);
+  console.log("post data:", dBase)
+});
+
+app.delete("/api/notes/:id", (req,res) => {
+  const noteId = dBase.find(note => note.id === parseInt(req.params.id));
+  const index = dBase.indexOf(noteId);
+  if(!noteId) 
+  return res.status(404).send("Note not available!");
+  
+  dBase.splice(index,1);
+  fs.writeFileSync("./Develop/db/db.json", JSON.stringify(dBase), "utf-8");
+  return res.json(true);
+});
+
+//My '404' response if indes page not found
 app.get("*", (req, res) => {
     res.json ("Page not found, SAVE FERRIS");
 });
-
 
 app.listen(PORT, () => {
     console.log ("app is working at port:", PORT);
 });
 
-//router.get()
-//router.post()
 
